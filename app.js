b3156c00b379a1165faa68569d84c399b15887b6
jhunderdog <33382water@gmail.com>
2021-07-22 07:53:06 +0900

    회원가입

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const postRouter = require('./routes/post');
+const userRouter = require('./routes/user');
 const db =require('./models');
 const app = express();
 
 db.sequelize.sync().then(() => { console.log('db연결성공');}).catch(console.error);
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.get('/', (req, res) => {
     res.send('hello express');
 });
+
 
 app.get('/', (req, res) => {
     res.send('hello api');
@@ -21,6 +26,7 @@ app.get('/posts', (req, res) => {
 });
 
 app.use('/post', postRouter);
+app.use('/user', userRouter);
 
 app.listen(3065, () => {
     console.log('서버 실행중!');
