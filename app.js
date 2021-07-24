ef45e2c09104fa7beb8229558afc3298b698601a
jhunderdog <33382water@gmail.com>
2021-07-24 09:25:00 +0900

    로그인passport

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const cors = require('cors');
 const postRouter = require('./routes/post');
 const userRouter = require('./routes/user');
 const db =require('./models');
+const passportConfig = require('./passport');
 const app = express();
 
 db.sequelize.sync().then(() => { console.log('db연결성공');}).catch(console.error);
-
+passportConfig();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const passport = require('passport');
 const { User } = require('../models');
 const router = express.Router();
+
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if (err) {
+            console.error(err);
+            return next(err);
+        }
+        if (info) {
+            return res.status(401).send(info.reason);
+        }
+        return req.login(user, async (loginErr) => {
+            if (loginErr){
+                console.error(loginErr);
+                return next(loginErr);
+            }
+            return res.json(user);
+        })
+    })(req, res, next);
+});
+//POST/user/login
 
 router.post('/', async (req, res, next) => {
     try {
