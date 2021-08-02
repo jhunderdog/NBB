b9dc6ca202673a6b6a0543ce37e29fc80571ede0
jhunderdog <33382water@gmail.com>
2021-08-02 20:48:37 +0900

    hashtags

--- routes/post.js
+++ routes/post.js
@@ -2,7 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-const { Post, Image, Comment, User } = require('../models');
+const { Post, Image, Comment, User, Hashtag } = require('../models');
 const { isLoggedIn } = require('./middlewares');
 const router = express.Router();
 try {
@@ -28,10 +28,17 @@ const upload = multer({
 
 router.post('/', isLoggedIn, upload.none(),  async (req, res, next) => {
     try {
+        const hashtags = req.body.content.match(/#[^\s#]+/g);
         const post = await Post.create({
             content: req.body.content,
             UserId: req.user.id,
         });
+        if (hashtags) {
+            const result = await Promise.all(hashtags.map((tag) => Hashtag.findOrCreate({ 
+                where: {name: tag.slice(1).toLowerCase()}
+            }))); // [[노드], true], [리액트, true]]
+            await post.addHashtags(result.map((v) => v[0]));
+        }
         if (req.body.image) {
             if (Array.isArray(req.body.image)) {// [111.png, 2222.png]//
                 const images = await Promise.all(req.body.image.map((image) => Image.create({src: image})));
